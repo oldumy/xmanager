78a6e2f50e28eb2417cc44880c397f0b707b79e1
Online Dummy <oldumy@gmail.com>
2010-09-07 17:16:16 +0800

    add mock users and roles

--- spec/factories/role_factory.rb
+++ spec/factories/role_factory.rb
@@ -5,9 +5,19 @@ Factory.define :administrators, :class => Role do |a|
   a.description 'Administrators of XManager' 
 end
 
+Factory.define :product_managers, :class => Role do |pm|
+  pm.role_name 'product_managers'
+  pm.description 'Product Managers' 
+end
+
 Factory.define :project_managers, :class => Role do |pm|
   pm.role_name 'project_managers'
-  pm.description 'Project Managers of XManager' 
+  pm.description 'Project managers of a project' 
+end
+
+Factory.define :developers, :class => Role do |d|
+  d.role_name 'developers'
+  d.description 'Developers of a project' 
 end
 
 Factory.define :role, :class => Role  do |r|
--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,8 +1,36 @@
 # encoding: utf-8
-Factory.define :role, :class => Role  do |r|
-  r.sequence(:role_name) { |n| "role#{n+1}" }
-  r.description "项目经理"
+Factory.define :admin, :class => User do |a|
+  a.login 'admin'
+  a.name 'Administrator'
+  a.password '123456'
+  a.password_confirmation '123456'
+  a.association :role, :factory => :administrators
 end
+
+Factory.define :product_manager, :class => User do |pm|
+  pm.login 'product_manager'
+  pm.name 'Product Manager'
+  pm.password '123456'
+  pm.password_confirmation '123456'
+  pm.association :role, :factory => :product_managers
+end
+
+Factory.define :project_manager, :class => User do |pm|
+  pm.login 'project_manager'
+  pm.name 'Project Manager'
+  pm.password '123456'
+  pm.password_confirmation '123456'
+  pm.association :role, :factory => :project_managers
+end
+
+Factory.define :developer, :class => User do |d|
+  d.login 'developer'
+  d.name 'Developer'
+  d.password '123456'
+  d.password_confirmation '123456'
+  d.association :role, :factory => :developers
+end
+
 Factory.define :user, :class => User  do |u|
   u.sequence(:login) { |n| "arthur#{n+1}" }
   u.name "arthurzhou"
